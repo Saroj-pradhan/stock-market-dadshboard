9dd6f3903dcb6d53e1cc5daf78bcc39fff853013
saroj <sarojpradhan7655@gmail.com>
2025-07-19 11:15:03 +0530

    setting up project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-amber-300'>
+    setting things up 
+       </div>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,8 @@
+const Dashboard = ()=>{
+    return(
+        <div>
+            hay this is dashboard
+        </div>
+    )
+}
+export default Dashboard;
