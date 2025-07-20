efb1879bf506156c1f74645f0fbfa5da80f9eefe
saroj <sarojpradhan7655@gmail.com>
2025-07-20 20:49:14 +0530

    resolved  image issue

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,10 +1,10 @@
-
+import logo from "../assets/images/logo.png"
 function Nav() {
   return (
     <div className='flex bg-gray-900 pl-3 pr-3 pt-2  pb-2  justify-between items-center text-white '>
     <div className="flex">
         <div>
-            <img  src="../src\assets\images\financial-profit.png" className="mr-3" width={50} height={50} alt="" srcset="" />
+            <img  src={logo} className="mr-3" width={50} height={50} alt="" srcset="" />
         </div>
         <div>
        <h1 className="text-2xl">StockMarket Dashboard</h1> 
